ea79ea081f5a332fcc41da6578f7688b70df8ea1
markov76 <101133846+markov76@users.noreply.github.com>
2022-11-05 07:33:18 +0200

    korjailtu

--- inc/funktions.php
+++ inc/funktions.php
@@ -1,7 +1,7 @@
 <?php
 
 function openDb() {
-    $db = new PDO('mysql:host=localhost;dbname=palautettava3;charcet=utf8','root', '');
+    $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charcet=utf8','root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     return $db;
 }
--- palautettava3.sql
+++ palautettava3.sql
@@ -7,6 +7,6 @@ create table item (
     id int primary key auto_increment,
     description varchar(255) not null,
     amount smallint unsigned not null
-):
+);
 
 insert into item (description,amount) values ('Test item',1);
